cd352c978cbbd85808c3394d57829d77969c22fa
Diego <98026092+dllack@users.noreply.github.com>
2022-02-15 21:31:20 -0300

    Soma

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma();
+        }
+
+        static void Soma()
+        {
+            Console.WriteLine("Primeiro valor:");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            double resultado = v1 + v2;
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+
+            Console.ReadKey();
+        }
+    }
+}
